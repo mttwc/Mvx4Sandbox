106b2b070de8bc6c9254474a0f4b640e88e5be72
mttwc <github@mttw.xyz>
2016-04-23 17:02:47 -0700

    Added ITesseractApi objects to each of the supported (Droid and iOS) ioc containers

--- Mvx4Sandbox/Mvx4Sandbox.Droid/Setup.cs
+++ Mvx4Sandbox/Mvx4Sandbox.Droid/Setup.cs
@@ -2,6 +2,9 @@ using Android.Content;
 using MvvmCross.Droid.Platform;
 using MvvmCross.Core.ViewModels;
 using MvvmCross.Platform.Platform;
+using MvvmCross.Platform;
+using Tesseract;
+using Tesseract.Droid;
 
 namespace Mvx4Sandbox.Droid
 {
@@ -9,6 +12,12 @@ namespace Mvx4Sandbox.Droid
     {
         public Setup(Context applicationContext) : base(applicationContext)
         {
+        }
+
+        protected override void InitializeFirstChance()
+        {
+            Mvx.RegisterSingleton<ITesseractApi>(new TesseractApi(ApplicationContext, AssetsDeployment.OncePerVersion));
+            base.InitializeFirstChance();
         }
 
         protected override IMvxApplication CreateApp()
--- Mvx4Sandbox/Mvx4Sandbox.iOS/Setup.cs
+++ Mvx4Sandbox/Mvx4Sandbox.iOS/Setup.cs
@@ -1,7 +1,10 @@
 using MvvmCross.Core.ViewModels;
 using MvvmCross.iOS.Platform;
 using MvvmCross.iOS.Views.Presenters;
+using MvvmCross.Platform;
 using MvvmCross.Platform.Platform;
+using Tesseract;
+using Tesseract.iOS;
 using UIKit;
 
 namespace Mvx4Sandbox.iOS
@@ -16,6 +19,12 @@ namespace Mvx4Sandbox.iOS
         public Setup(MvxApplicationDelegate applicationDelegate, IMvxIosViewPresenter presenter)
             : base(applicationDelegate, presenter)
         {
+        }
+
+        protected override void InitializeFirstChance()
+        {
+            Mvx.RegisterSingleton<ITesseractApi>(new TesseractApi());
+            base.InitializeFirstChance();
         }
 
         protected override IMvxApplication CreateApp()
